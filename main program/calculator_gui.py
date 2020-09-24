804e0435f97276f294e97ce95f23b4bad4097e9f
Harry Baker <62488187+haz-baker@users.noreply.github.com>
2020-09-24 22:07:00 +0100

    converting to pyslide2

--- main program/calculator_gui.py
+++ main program/calculator_gui.py
@@ -7,7 +7,11 @@
 # WARNING! All changes made in this file will be lost!
 
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import QFile, QIODevice
+
 from calc_functions import *
 
 class Ui_Dialog(object):
@@ -374,10 +378,28 @@ class Ui_Dialog(object):
 
 if __name__ == "__main__":
     import sys
+
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
     app = QtWidgets.QApplication(sys.argv)
+
+    ui_file_name = "calculator_layout.ui"
+    ui_file = QFile(ui_file_name)
+    if not ui_file.open(QIODevice.ReadOnly):
+        print("Cannot open {}: {}".format(ui_file_name, ui_file.errorString()))
+        sys.exit(-1)
+    loader = QUiLoader()
+    dialog = loader.load(ui_file)
+    ui_file.close()
+    if not dialog:
+        print(loader.errorString())
+        sys.exit(-1)
+    dialog.show()
+    sys.exit(app.exec_())
+"""
     Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
+"""
 
